ea42091637a09417514111efc14af75e79e67f41
averbuh <averbuh.alex.in@gmail.com>
2024-04-10 09:50:02 +0300

    Add configuration files for ESLint, Prettier, and VSCode extensions, as well as a basic project structure with Vue 3 and Vite setup. Also include a simple index.html and a jsconfig.json for path aliases setup.

---
+++ src/components/Recipes.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="recipes">
+    <h1>Recipes</h1>
+    <p>Info about recipes</p>
+  </div>
+</template>
+
+<style scoped>
+.recipes {
+  font-size: 10px;
+}
+</style>
